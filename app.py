0a1856eafea3d55bb8145dd0b53356521b9f1433
casmircodes <72873224+casmircodes@users.noreply.github.com>
2025-07-19 17:35:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ CORS(app, origins=[frontend_url])  # Allow requests from frontend (Netlify)
 
 ind = 0
 
-keys = [os.environ.get("GOOGLE_API_KEY"), os.environ.get("GOOGLE_API_KEY1"), os.environ.get("GOOGLE_API_KEY2"), os.environ.get("GOOGLE_API_KEY3"), os.environ.get("GOOGLE_API_KEY4"), os.environ.get("GOOGLE_API_KEY5"), os.environ.get("GOOGLE_API_KEY6"), os.environ.get("GOOGLE_API_KEY7"), os.environ.get("GOOGLE_API_KEY8"), os.environ.get("GOOGLE_API_KEY9")]
+keys = [os.environ.get("GOOGLE_API_KEY"), os.environ.get("GOOGLE_API_KEY1"), os.environ.get("GOOGLE_API_KEY2"), os.environ.get("GOOGLE_API_KEY3"), os.environ.get("GOOGLE_API_KEY4"), os.environ.get("GOOGLE_API_KEY5"), os.environ.get("GOOGLE_API_KEY6"), os.environ.get("GOOGLE_API_KEY7"), os.environ.get("GOOGLE_API_KEY8"), os.environ.get("GOOGLE_API_KEY9"), os.environ.get("GOOGLE_API_KEY10")]
 
 # Configure the Gemini API key from the environment variable
 GOOGLE_API_KEY = keys[ind]
